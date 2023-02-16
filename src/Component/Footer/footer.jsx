f83e02ca6ada58918b5530a466235f329079f9a4
Chloé halloin <halloinchloe@gmail.com>
2023-02-16 13:31:37 +0100

    footer

--- src/Component/Footer/footer.jsx
+++ src/Component/Footer/footer.jsx
@@ -1,18 +1,102 @@
 import React from "react";
-import ItemsContainer from "./ItemsContainer";
+import logo from "../../assets/logo.svg";
 
-const Footer = () => {
+function Footer() {
   return (
-    <footer className="bg-black text-white text-center">
-      <ItemsContainer />
-      <div
-        className="grid grid-cols-1 sm:grid-cols-1 lg:grid-cols-1 gap-10
-      text-center pt-2 text-gray-400 text-sm pb-8"
-      >
-        <span>© 2023 iVie. All rights reserved.</span>
+    <div className="main-footer mr-0 bg-black text-white">
+      <div className="w-full py-8">
+        <div className="grid grid-cols-1 mr-0 sm:grid-cols-3 gap-8 text-center">
+          {/* Column1 */}
+          <div>
+            <h4> </h4>
+            <ul className="list-none text-sm">
+              <li className="pb-2">Legal mention</li>
+              <li className="pb-2">Privacy Policy</li>
+              <li className="pb-2">Terms of Use</li>
+            </ul>
+          </div>
+          {/* Column2 */}
+          <div className="text-center">
+            <h4>
+              <img
+                src={logo}
+                className="inline h-6  pb-3 sm:h-9"
+                alt="ivie Logo"
+              />
+            </h4>
+            <ul className=" list-none text-sm">
+              <li className="pb-2">Discovering new worlds</li>
+              <li className="inline pb-2">
+                <div className="grid grid-cols-4 gap-4">
+                  <div>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24"
+                      fill="currentColor"
+                      class="w-6 h-6 inline"
+                    >
+                      <path d="M12 2.25a.75.75 0 01.75.75v2.25a.75.75 0 01-1.5 0V3a.75.75 0 01.75-.75zM7.5 12a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM18.894 6.166a.75.75 0 00-1.06-1.06l-1.591 1.59a.75.75 0 101.06 1.061l1.591-1.59zM21.75 12a.75.75 0 01-.75.75h-2.25a.75.75 0 010-1.5H21a.75.75 0 01.75.75zM17.834 18.894a.75.75 0 001.06-1.06l-1.59-1.591a.75.75 0 10-1.061 1.06l1.59 1.591zM12 18a.75.75 0 01.75.75V21a.75.75 0 01-1.5 0v-2.25A.75.75 0 0112 18zM7.758 17.303a.75.75 0 00-1.061-1.06l-1.591 1.59a.75.75 0 001.06 1.061l1.591-1.59zM6 12a.75.75 0 01-.75.75H3a.75.75 0 010-1.5h2.25A.75.75 0 016 12zM6.697 7.757a.75.75 0 001.06-1.06l-1.59-1.591a.75.75 0 00-1.061 1.06l1.59 1.591z" />
+                    </svg>
+                  </div>
+                  <div>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24"
+                      fill="currentColor"
+                      class="w-6 h-6 inline"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M9.315 7.584C12.195 3.883 16.695 1.5 21.75 1.5a.75.75 0 01.75.75c0 5.056-2.383 9.555-6.084 12.436A6.75 6.75 0 019.75 22.5a.75.75 0 01-.75-.75v-4.131A15.838 15.838 0 016.382 15H2.25a.75.75 0 01-.75-.75 6.75 6.75 0 017.815-6.666zM15 6.75a2.25 2.25 0 100 4.5 2.25 2.25 0 000-4.5z"
+                        clip-rule="evenodd"
+                      />
+                      <path d="M5.26 17.242a.75.75 0 10-.897-1.203 5.243 5.243 0 00-2.05 5.022.75.75 0 00.625.627 5.243 5.243 0 005.022-2.051.75.75 0 10-1.202-.897 3.744 3.744 0 01-3.008 1.51c0-1.23.592-2.323 1.51-3.008z" />
+                    </svg>
+                  </div>
+                  <div>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24"
+                      fill="currentColor"
+                      class="w-6 h-6 inline"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M9 4.5a.75.75 0 01.721.544l.813 2.846a3.75 3.75 0 002.576 2.576l2.846.813a.75.75 0 010 1.442l-2.846.813a3.75 3.75 0 00-2.576 2.576l-.813 2.846a.75.75 0 01-1.442 0l-.813-2.846a3.75 3.75 0 00-2.576-2.576l-2.846-.813a.75.75 0 010-1.442l2.846-.813A3.75 3.75 0 007.466 7.89l.813-2.846A.75.75 0 019 4.5zM18 1.5a.75.75 0 01.728.568l.258 1.036c.236.94.97 1.674 1.91 1.91l1.036.258a.75.75 0 010 1.456l-1.036.258c-.94.236-1.674.97-1.91 1.91l-.258 1.036a.75.75 0 01-1.456 0l-.258-1.036a2.625 2.625 0 00-1.91-1.91l-1.036-.258a.75.75 0 010-1.456l1.036-.258a2.625 2.625 0 001.91-1.91l.258-1.036A.75.75 0 0118 1.5zM16.5 15a.75.75 0 01.712.513l.394 1.183c.15.447.5.799.948.948l1.183.395a.75.75 0 010 1.422l-1.183.395c-.447.15-.799.5-.948.948l-.395 1.183a.75.75 0 01-1.422 0l-.395-1.183a1.5 1.5 0 00-.948-.948l-1.183-.395a.75.75 0 010-1.422l1.183-.395c.447-.15.799-.5.948-.948l.395-1.183A.75.75 0 0116.5 15z"
+                        clip-rule="evenodd"
+                      />
+                    </svg>
+                  </div>
+                  <div>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24"
+                      fill="currentColor"
+                      className="w-6 h-6 inline"
+                    >
+                      <path d="M11.645 20.91l-.007-.003-.022-.012a15.247 15.247 0 01-.383-.218 25.18 25.18 0 01-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0112 5.052 5.5 5.5 0 0116.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 01-4.244 3.17 15.247 15.247 0 01-.383.219l-.022.012-.007.004-.003.001a.752.752 0 01-.704 0l-.003-.001z" />
+                    </svg>
+                  </div>
+                </div>
+              </li>
+            </ul>
+          </div>
+          {/* Column3 */}
+          <div>
+            <h4> </h4>
+            <ul className="list-none text-sm">
+              <li className="pb-2">Press</li>
+              <li className="pb-2">Jobs</li>
+              <li className="pb-2">Contact</li>
+            </ul>
+          </div>
+        </div>
+        <div className="text-center pt-8 text-gray-400 text-sm">
+          <span>© 2023 iVie. All rights reserved.</span>
+        </div>
       </div>
-    </footer>
+    </div>
   );
-};
+}
 
 export default Footer;
